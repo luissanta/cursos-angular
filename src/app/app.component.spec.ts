8e3c45ca01becbd1a8524810809a7d9672b5ce21
Luis <luisfernandosantaospina@gmail.com>
2022-10-27 16:35:57 -0500

    Crear estructura de proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'mynewapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mynewapp');
+    expect(app.title).toEqual('app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mynewapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app app is running!');
   });
 });
